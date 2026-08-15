99388805558a26e8d9255b6df35ce86a35c9712f
Adam-and-Eve <base.github.account@yandex.ru>
2026-08-15 12:56:18 +0300

    feat(front-ui-service): покрыть тестами конфигурации, клиенты шлюза и контроллеры

---
+++ front-ui-service/src/test/java/ru/yandex/practicum/bank/frontui/FrontUiApplicationTest.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.bank.frontui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+/**
+ * <summary>
+ * Интеграционный тест для проверки корректности загрузки контекста Spring-приложения (FrontUiApplication).
+ * Подтверждает успешную инициализацию всех бинов и конфигураций микросервиса account-service.
+ * </summary>
+ **/
+@SpringBootTest
+public class FrontUiApplicationTest {
+
+    // region Fields
+
+    /**
+     * <summary>
+     * Мок-объект {@link JwtDecoder} для имитации работы OAuth2 Resource Server
+     * и исключения внешних вызовов к Identity Provider (Keycloak) при поддомене контекста.
+     * </summary>
+     **/
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
+    // endregion
+
+    // region Tests
+
+    /**
+     * <summary>
+     * Проверяет, что контекст Spring-приложения успешно поднимается без ошибок конфигурации и сбоев инициализации бинов.
+     * </summary>
+     **/
+    @Test
+    public void contextLoads() {
+
+    }
+
+    // endregion
+}
